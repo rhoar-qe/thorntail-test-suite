17c99e20c4f365730bb24c54e8a270aaa033f536
Ladislav Thon <lthon@redhat.com>
2018-04-27 19:54:09 +0200

    add a remote messaging test to the "full" hollow JAR test

--- hollow-jar/full/src/test/java/org/wildfly/swarm/ts/hollow/jar/full/FullHollowJarIT.java
+++ hollow-jar/full/src/test/java/org/wildfly/swarm/ts/hollow/jar/full/FullHollowJarIT.java
@@ -14,6 +14,10 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import org.wildfly.swarm.ts.hollow.jar.full.ejb.remote.RemoteEjb;
 
+import javax.jms.ConnectionFactory;
+import javax.jms.JMSContext;
+import javax.jms.JMSProducer;
+import javax.jms.Queue;
 import javax.mail.internet.MimeMultipart;
 import javax.naming.Context;
 import javax.naming.InitialContext;
@@ -92,6 +96,38 @@ public class FullHollowJarIT {
             ctx = new InitialContext(jndiProps);
             RemoteEjb service = (RemoteEjb) ctx.lookup("ejb:/ts-hollow-jar-full-1.0.0-SNAPSHOT/RemoteEjbImpl!org.wildfly.swarm.ts.hollow.jar.full.ejb.remote.RemoteEjb");
             assertThat(service.method()).isEqualTo("remote ejb method");
+        } finally {
+            if (ctx != null) {
+                ctx.close();
+            }
+        }
+    }
+
+    @Test
+    public void testRemoteMessaing() throws NamingException {
+        Hashtable<String, Object> jndiProps = new Hashtable<>();
+        jndiProps.put(Context.INITIAL_CONTEXT_FACTORY, "org.wildfly.naming.client.WildFlyInitialContextFactory");
+        jndiProps.put(Context.PROVIDER_URL, "remote+http://localhost:8080");
+
+        Context ctx = null;
+        try {
+            ctx = new InitialContext(jndiProps);
+            ConnectionFactory connectionFactory = (ConnectionFactory) ctx.lookup("jms/RemoteConnectionFactory");
+            Queue queue = (Queue) ctx.lookup("jms/queue/my-queue");
+
+            try (JMSContext jms = connectionFactory.createContext("test-user", "test-user-password")) {
+                JMSProducer producer = jms.createProducer();
+                producer.send(queue, "Hello 1");
+                producer.send(queue, "Hello 2");
+            }
+
+            await().atMost(1, TimeUnit.SECONDS).untilAsserted(() -> {
+                String response = Request.Get("http://localhost:8080/messaging?operation=results").execute().returnContent().asString();
+                assertThat(response).contains("Hello 1");
+                assertThat(response).contains("Hello 2");
+            });
+
+            ctx.close();
         } finally {
             if (ctx != null) {
                 ctx.close();
