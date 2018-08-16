867c11fb1bd8a7fbbe5c2f60eea201f266ca306f
Juan Gonzalez <juagonza@redhat.com>
2018-08-16 10:43:54 +0200

    add MicroProfile Rest Client 1.0 tests

---
+++ microprofile/microprofile-rest-client-1.0/src/main/java/org/wildfly/swarm/ts/microprofile/rest/client/v10/RestSimpleResourceRestClient.java
@@ -0,0 +1,13 @@
+package org.wildfly.swarm.ts.microprofile.rest.client.v10;
+
+import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+@RegisterRestClient
+public interface RestSimpleResourceRestClient {
+    @GET
+    @Path("/rest/simple")
+    String simpleOperation();
+}
