1849aaed07c88efb66eeef23853206dcd9fa5f95
Juan Gonzalez <juagonza@redhat.com>
2018-08-16 10:12:56 +0200

    add MicroProfile Config 1.2 tests

---
+++ microprofile/microprofile-config-1.2/src/test/java/org/wildfly/swarm/ts/microprofile/config/v12/MicroProfileConfig12Test.java
@@ -0,0 +1,25 @@
+package org.wildfly.swarm.ts.microprofile.config.v12;
+
+import org.apache.http.client.fluent.Request;
+import org.jboss.arquillian.container.test.api.RunAsClient;
+import org.jboss.arquillian.junit.Arquillian;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.wildfly.swarm.arquillian.DefaultDeployment;
+
+import java.io.IOException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(Arquillian.class)
+@DefaultDeployment
+public class MicroProfileConfig12Test {
+    @Test
+    @RunAsClient
+    public void test() throws IOException {
+        String response = Request.Get("http://localhost:8080/").execute().returnContent().asString();
+        assertThat(response).isEqualTo(
+                "arrayproperty programatic: [element1, element2, element3, element41\\,element42]\n" +
+                "arrayproperty injected list: element1,element2,element3,element41\\,element42\n");
+    }
+}
