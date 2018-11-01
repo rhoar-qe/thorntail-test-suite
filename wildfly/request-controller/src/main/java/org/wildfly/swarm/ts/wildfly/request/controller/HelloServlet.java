0c9cbcc983a94c2dc58f3d16c3ff7acbd8e16e22
Ladislav Thon <lthon@redhat.com>
2018-11-01 12:37:14 +0100

    remove unnecessary import

--- wildfly/request-controller/src/main/java/org/wildfly/swarm/ts/wildfly/request/controller/HelloServlet.java
+++ wildfly/request-controller/src/main/java/org/wildfly/swarm/ts/wildfly/request/controller/HelloServlet.java
@@ -1,6 +1,5 @@
 package org.wildfly.swarm.ts.wildfly.request.controller;
 
-import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
