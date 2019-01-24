5b841d80af6aca28c297905de53b46e61d85bcbd
Ladislav Thon <lthon@redhat.com>
2019-01-24 17:24:41 +0100

    update to Java 11

--- javaee/jaxrs-cdi/src/main/java/org/wildfly/swarm/ts/javaee/jaxrs/cdi/HelloService.java
+++ javaee/jaxrs-cdi/src/main/java/org/wildfly/swarm/ts/javaee/jaxrs/cdi/HelloService.java
@@ -1,9 +1,6 @@
 package org.wildfly.swarm.ts.javaee.jaxrs.cdi;
 
 import javax.enterprise.context.ApplicationScoped;
-import javax.xml.bind.annotation.XmlAttribute;
-import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlRootElement;
 
 @ApplicationScoped
 public class HelloService {
