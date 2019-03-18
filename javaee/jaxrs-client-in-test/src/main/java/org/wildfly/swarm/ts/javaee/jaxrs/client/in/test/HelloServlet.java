b666fe45a0f74a1d34c74f8a449331070ac2508f
Ladislav Thon <lthon@redhat.com>
2019-03-18 11:48:51 +0100

    rename "jaxrs-client" to "jaxrs-client-in-test"
    This test suite module only uses JAX-RS Client in a test class.
    We need a test suite module that verifies JAX-RS Client usage
    as part of the actual application, and this test suite module
    should be called `jaxrs-client`. Hence the rename.

--- javaee/jaxrs-client-in-test/src/main/java/org/wildfly/swarm/ts/javaee/jaxrs/client/in/test/HelloServlet.java
+++ javaee/jaxrs-client-in-test/src/main/java/org/wildfly/swarm/ts/javaee/jaxrs/client/in/test/HelloServlet.java
@@ -1,4 +1,4 @@
-package org.wildfly.swarm.ts.javaee.jaxrs.client;
+package org.wildfly.swarm.ts.javaee.jaxrs.client.in.test;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
