8cf130f5ff2bb995ae0bbbf9e1b44e17f915ad36
Ladislav Thon <lthon@redhat.com>
2019-04-09 12:49:44 +0200

    formatting

--- microprofile/microprofile-jwt-1.1/src/test/java/org/wildfly/swarm/ts/microprofile/jwt/v11/ConfigLocationJwt11Test.java
+++ microprofile/microprofile-jwt-1.1/src/test/java/org/wildfly/swarm/ts/microprofile/jwt/v11/ConfigLocationJwt11Test.java
@@ -11,10 +11,8 @@ import org.junit.runner.RunWith;
 
 @RunWith(Arquillian.class)
 public class ConfigLocationJwt11Test extends AbstractConfigJwt11Test {
-
     @Deployment
     public static Archive<?> deployment() {
-
         return ShrinkWrap.create(WebArchive.class)
                 .addPackage(ConfigLocationJwt11Test.class.getPackage())
                 .addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml")
