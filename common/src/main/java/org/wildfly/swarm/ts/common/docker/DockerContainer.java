72084c33950b3ccd6b372838b077afbfc4e0f608
Juan G <juagonza@redhat.com>
2019-05-09 12:00:50 +0200

    stabilize Docker-based tests using Jaeger

--- common/src/main/java/org/wildfly/swarm/ts/common/docker/DockerContainer.java
+++ common/src/main/java/org/wildfly/swarm/ts/common/docker/DockerContainer.java
@@ -1,19 +1,26 @@
 package org.wildfly.swarm.ts.common.docker;
 
+import org.fusesource.jansi.Ansi;
+
 import java.io.IOException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.TimeUnit;
 
 public class DockerContainer {
+    private final String name;
     private final String containerId;
     private final ExecutorService outputPrinter;
 
-    DockerContainer(String containerId, ExecutorService outputPrinter) {
+    DockerContainer(String name, String containerId, ExecutorService outputPrinter) {
+        this.name = name;
         this.containerId = containerId;
         this.outputPrinter = outputPrinter;
     }
 
     public void stop() throws IOException, InterruptedException {
+        System.out.println(Ansi.ansi().reset().a("Stopping container ").fgCyan().a(name).reset()
+                .a(" with ID ").fgYellow().a(containerId).reset());
+
         new ProcessBuilder()
                 .command("docker", "stop", containerId)
                 .start()
