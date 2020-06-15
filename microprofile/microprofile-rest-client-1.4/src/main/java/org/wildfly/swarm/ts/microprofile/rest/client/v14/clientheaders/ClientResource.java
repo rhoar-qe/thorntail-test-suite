4bb401bad5864da5271fc95bdbd1436b4c72dd75
penehyba <penehyba@redhat.com>
2020-06-15 14:55:38 +0200

    [THORN-2561] RestClient 1.4

---
+++ microprofile/microprofile-rest-client-1.4/src/main/java/org/wildfly/swarm/ts/microprofile/rest/client/v14/clientheaders/ClientResource.java
@@ -0,0 +1,20 @@
+package org.wildfly.swarm.ts.microprofile.rest.client.v14.clientheaders;
+
+import java.net.URL;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+import org.eclipse.microprofile.rest.client.RestClientBuilder;
+
+@Path("/client")
+public class ClientResource {
+
+    @GET
+    public String headers() throws Exception {
+        ClientHeadersClient client = RestClientBuilder.newBuilder()
+                .baseUrl(new URL("http://localhost:8080"))
+                .build(ClientHeadersClient.class);
+        return client.getHeaders();
+    }
+}
