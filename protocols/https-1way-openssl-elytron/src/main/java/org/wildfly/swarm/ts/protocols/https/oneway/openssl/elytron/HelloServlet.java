6531d75cf07630ca1c0034b5fc7d48a45c579955
Ladislav Thon <lthon@redhat.com>
2020-10-07 13:32:27 +0200

    add tests for WildFly OpenSSL
    These tests are copied from `https-1way[-elytron]` tests,
    the only modification is enabling WildFly OpenSSL.

---
+++ protocols/https-1way-openssl-elytron/src/main/java/org/wildfly/swarm/ts/protocols/https/oneway/openssl/elytron/HelloServlet.java
@@ -0,0 +1,15 @@
+package org.wildfly.swarm.ts.protocols.https.oneway.openssl.elytron;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/")
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.getWriter().print("Hello on port " + req.getLocalPort() + ", secure: " + req.isSecure());
+    }
+}
